9c96fdb1a80754c331229b3d14106337491edc7b
Ethan502 <esmit502@byu.edu>
2024-01-17 17:49:06 -0700

    whoohoo

--- ch2/2_1.py
+++
@@ -1,9 +0,0 @@
-import gtsam
-from gtsam import DiscreteDistribution
-import gtbook
-from gtbook.discrete import Variables
-from gtbook.display import show
-
-VARIABLES = Variables()
-def pretty(obj):
-    return gtbook.display.pretty(obj, VARIABLES)
